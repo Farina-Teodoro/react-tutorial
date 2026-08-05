632ef4e7c35a018d9b55634821168378962c7340
Teodoro Machado Farina <teodoro.mf17@gmail.com>
2026-08-05 11:15:01 -0300

    Initial commit

---
+++ src/Paragrafo.jsx
@@ -0,0 +1,11 @@
+import messi from './assets/messi-foda.png'
+function Paragrafo() {
+    return (
+    <>
+        <p>Parágrafo explicativo</p>
+        <p><img src={messi}/></p>
+    </>
+)     
+}
+
+export default Paragrafo
