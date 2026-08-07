5785c405c6d9bb7a50477a2b83ef20f93def94a3
Teodoro Machado Farina <teodoro.mf17@gmail.com>
2026-08-07 08:28:12 -0300

    07-08

--- src/Paragrafo.jsx
+++ src/Paragrafo.jsx
@@ -1,11 +1,27 @@
-import messi from './assets/messi-foda.png'
-function Paragrafo() {
+import { useState } from "react"
+
+const Paragrafo = (props) => {
+    const [pontos, setPontos] = useState(0);
+
+    const aumentarPontos = () => {
+        setPontos(pontos + 1);
+    }
+
+    const diminuirPontos = () => { 
+        if (pontos > 0) {
+            setPontos(pontos - 1);
+        }
+    }
+
     return (
-    <>
-        <p>Parágrafo explicativo</p>
-        <p><img src={messi} width={150} height={150}/></p>   
-    </>
-)     
+        <div>
+            <p>{props.nome} tem {pontos} pontos!
+                <button onClick={aumentarPontos}>+</button>
+                <button onClick={diminuirPontos}>-</button>
+            </p>
+            
+        </div>
+    );
 }
 
-export default Paragrafo
+export default Paragrafo   
