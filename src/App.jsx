9b014b0beed65ac9d8eeee1aff26d4db79ec178f
Teodoro Machado Farina <teodoro.mf17@gmail.com>
2026-08-28 07:37:19 -0300

    0826

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,13 @@ function App() {
   const [num] = useState(1)
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Number is {num}
-          </p>
-        </div>
-        <Paragrafo nome="Theomajs" />
-        <Paragrafo nome="Jpinto" />
-        <CaixaTexto/>
-      </section>
-
-      <div className="ticks"></div>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <ThemeProvider>
+      <div>
+        <nav>Menu de navegação</nav>
+        <ThemeToggleButton/>
+      </div>
+    </ThemeProvider>
   )
 }
 
-export default App
+export default App;
